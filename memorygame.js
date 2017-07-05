9d303051162ca6463d7c8b26a76bd01bb5c91c0d
yanniznik <account@yananas.com>
2017-07-05 14:17:10 +0300

    Added new functionalities and themes

--- memorygame.js
+++ memorygame.js
@@ -1,109 +1,174 @@
-	// SETTING VARIABLES
-	var allCards = [];
-	var selectedCards = [];
-	var toVictory = 0;
-	var flipCounter = 0;
+		var theme = 1;
+		var allCards = [];
+		var selectedCards = [];
+		var toVictory = 0;
+		var flipCounter = 0;
+		var tryCounter = 0;
+		var score = 0;
+		var difficulty = 12;
 
 	// STARTING GAME
 
 	function startGame() {
-	    setUp();
-	    randomize();
+		cleanUp();
+		setUp();
+		randomize();
+		newGame();
+		changeDifficulty();
+	}
+
+	// GENERAL CLEAN UP
+
+	function cleanUp() {
+		document.getElementById("container").innerHTML = '';
+		document.getElementById("won").classList.remove("showme");
+		allCards = [];
+		selectedCards = [];
+		toVictory = 0;
+		flipCounter = 0;
+		tryCounter = 0;
+		score = 0;
+	}
+
+	// THEME HANDLER
+
+	function themeHandler() {
+			var sel = document.getElementById("select");
+			theme = sel.options[sel.selectedIndex].value;
+	}
+
+	// COUNTERS 
+
+	function counter() {
+		document.getElementById("tries").innerHTML = tryCounter;
+		document.getElementById("score").innerHTML = score;
+	}
+
+	// DIFFICULTY HANDLER
+
+	function changeDifficulty() {
+		var buttons = document.getElementsByClassName("difficulty");
+		for (var i = 0; i < buttons.length; i++) {
+			buttons[i].addEventListener("click", function() {
+				difficulty = this.value;
+				var selectedElements = document.getElementsByClassName("selected");
+				for (var i = 0; i < selectedElements.length; i++) {
+					selectedElements[i].classList.remove("selected")
+				}
+				this.classList.add("selected")
+			})
+		}
+
+	}
+
+	// NEW GAME
+
+	function newGame() {
+		document.getElementById("newgame").addEventListener("click", function() {
+			if (confirm("Are you sure you want to restart?")) {
+				themeHandler();
+				startGame();
+			}
+		})
 	}
 
 	// CREATING CARDS
 
 	function setUp() {
-	    for (var i = 1; i < 12; i++) {
-	        allCards[i] = new card(i);
-	        allCards[i + 1] = new card(i);
-	        allCards[i].create()
-	        allCards[i + 1].create()
-	        allCards[i].addClick();
-	        allCards[i + 1].addClick();
-	        i++
-	    }
+		for (var i = 1; i < difficulty; i++) {
+			allCards[i] = new card(i);
+			allCards[i + 1] = new card(i);
+			allCards[i].create()
+			allCards[i + 1].create()
+			allCards[i].addClick();
+			allCards[i + 1].addClick();
+			i++
+		}
 	}
 
 	// RANDOMIZE BOARD
 
 	function randomize() {
-	    var cont = document.getElementById("container");
-	    for (var i = cont.children.length; i >= 0; i--) {
-	        cont.appendChild(cont.children[Math.random() * i | 0]);
-	    }
+		var cont = document.getElementById("container");
+		for (var i = cont.children.length; i >= 0; i--) {
+			cont.appendChild(cont.children[Math.random() * i | 0]);
+		}
 	}
 
 	// MAIN CARD OBJECT
 
 	function card(id) {
-	    var flipC = document.createElement("div");
-	    this.id = id;
-	    this.isFlipped = 0;
-	    this.isFound = 0;
+		var flipC = document.createElement("div");
+		this.id = id;
+		this.isFlipped = 0;
+		this.isFound = 0;
 
-	    this.flip = function() {
-	        this.isFlipped = 1;
-	        flipC.classList.add("hover");
-	        selectedCards.push(this.id);
-	        gameHandler();
-	    }
-	    this.unflip = function() {
-	        this.isFlipped = 0;
-	        flipC.classList.remove("hover");
-	    }
+		this.flip = function() {
+			this.isFlipped = 1;
+			flipC.classList.add("hover");
+			selectedCards.push(this.id);
+			gameHandler();
+		}
+		this.unflip = function() {
+			this.isFlipped = 0;
+			flipC.classList.remove("hover");
+		}
 
-	    this.create = function() {
-	        var mainC = document.createElement("div");
-	        mainC.setAttribute("class", "col-md-3");
-	        flipC.classList.add("flip-container");
-	        flipC.setAttribute("class", "flip-container")
-	        var flipper = document.createElement("div");
-	        flipper.setAttribute("class", "flipper");
-	        var front = document.createElement("div");
-	        front.setAttribute("class", "front");
-	        var back = document.createElement("div");
-	        back.setAttribute("class", "front");
-	        back.style.backgroundImage = "url('img/texture.jpg')";
-	        front.style.backgroundImage = "url('img/" + id + ".jpg')";
-	        flipper.appendChild(front);
-	        flipper.appendChild(back);
-	        flipC.appendChild(flipper);
-	        mainC.appendChild(flipC);
-	        document.getElementById("container").appendChild(mainC);
-	    }
-	    this.addClick = function() {
-	        flipC.addEventListener('click', this.flip.bind(this), false)
+		this.create = function() {
+			var mainC = document.createElement("div");
+			mainC.setAttribute("class", "col-md-3 col-xs-6");
+			flipC.classList.add("flip-container");
+			flipC.setAttribute("class", "flip-container")
+			var flipper = document.createElement("div");
+			flipper.setAttribute("class", "flipper");
+			var front = document.createElement("div");
+			front.setAttribute("class", "front");
+			var back = document.createElement("div");
+			back.setAttribute("class", "front");
+			back.style.backgroundImage = "url('img/" + theme + "/texture.jpg')";
+			front.style.backgroundImage = "url('img/" + theme + "/" + id + ".jpg')";
+			flipper.appendChild(front);
+			flipper.appendChild(back);
+			flipC.appendChild(flipper);
+			mainC.appendChild(flipC);
+			document.getElementById("container").appendChild(mainC);
+		}
+		this.addClick = function() {
+			flipC.addEventListener('click', this.flip.bind(this), false)
 
-	    }
+		}
 	}
 
 	// GAME HANDLER
 
 	function gameHandler() {
-	    flipCounter += 1;
+		flipCounter += 1;
 
-	    if (selectedCards[0] == selectedCards[1] && flipCounter == 2) {
-	        flipCounter = 0;
-	        toVictory += 2;
-	        allCards[selectedCards[0]].isFound = 1;
-	        allCards[selectedCards[1] + 1].isFound = 1;
-	        selectedCards = [];
-	    }
+		if (selectedCards[0] == selectedCards[1] && flipCounter == 2) {
+			flipCounter = 0;
+			toVictory += 2;
+			score =+1;
+			allCards[selectedCards[0]].isFound = 1;
+			allCards[selectedCards[1] + 1].isFound = 1;
+			selectedCards = [];
+		}
 
-	    if (toVictory == allCards.length - 1) {
-	        console.log("WON");
-	    } else if (selectedCards[0] != selectedCards[1] && flipCounter == 2) {
-	        flipCounter = 0;
-	        selectedCards = [];
-	        setTimeout(function() {
-	            for (var i = 1; i < allCards.length; i++) {
-	                if (allCards[i].isFlipped == 1 && allCards[i].isFound == 0) {
-	                    allCards[i].unflip();
+		if (toVictory == allCards.length - 1) {
+			console.log("WON");
+			document.getElementById("won").classList.add("showme");
+		} else if (selectedCards[0] != selectedCards[1] && flipCounter == 2) {
+			flipCounter = 0;
+			tryCounter += 1;
+			selectedCards = [];
+			setTimeout(function() {
+				for (var i = 1; i < allCards.length; i++) {
+	                if (allCards[i].isFlipped == 1 && allCards[i].isFound == 0) { // unflip only flipped cards that are not found
+	                	allCards[i].unflip();
 	                }
 	            }
 	        }, 1000)
-	    }
+		}
+		counter();
 	}
 
 	startGame();
